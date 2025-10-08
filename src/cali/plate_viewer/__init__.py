28d248ea1718fa0eb45f1ad20f8dad1700533837
fdrgsp <federico.gasparoli@gmail.com>
2025-10-08 12:53:29 -0400

    init

--- src/cali/plate_viewer/__init__.py
+++ src/cali/plate_viewer/__init__.py
@@ -1,3 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
 from ._plate_viewer import PlateViewer
+
+try:
+    __version__ = version("micromanager-gui")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+
+__author__ = "Federico Gasparoli"
+__email__ = "federico.gasparoli@gmail.com"
 
 __all__ = ["PlateViewer"]
