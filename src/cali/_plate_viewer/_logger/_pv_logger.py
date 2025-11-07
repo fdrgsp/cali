114a9317cf0a5c3aede6a7688fa607e52ca3103b
fdrgsp <federico.gasparoli@gmail.com>
2025-11-07 16:14:15 -0500

    cali logger

--- src/cali/_plate_viewer/_logger/_pv_logger.py
+++ src/cali/_plate_viewer/_logger/_pv_logger.py
@@ -1,14 +1,14 @@
 import logging
 from pathlib import Path
 
-LOGGER = logging.getLogger("analysis_logger")
+LOGGER = logging.getLogger("cali_logger")
 LOGGER.setLevel(logging.DEBUG)
 
 # Create formatter
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 # File handler
-log_file = Path(__file__).parent / "analysis_logger.log"
+log_file = Path(__file__).parent / "cali_logger.log"
 file_handler = logging.FileHandler(log_file)
 file_handler.setLevel(logging.DEBUG)
 file_handler.setFormatter(formatter)
