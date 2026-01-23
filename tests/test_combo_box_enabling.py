e0c152a3ae7cebf3144be049515b4f46b12a58ed
fdrgsp <federico.gasparoli@gmail.com>
2026-01-23 17:55:14 -0500

    fix tests

--- tests/test_combo_box_enabling.py
+++ tests/test_combo_box_enabling.py
@@ -109,9 +109,10 @@ def test_combo_disabled_with_only_run_id(
     _assert_pipeline_stages(widget, has_det=False, has_ext=False, has_ana=False)
 
     # All items should still be disabled
-    # (67 plots: spontaneous + evoked + sorted rising edges CCG + 2 CCG z-score plots
-    #  + 2 sorted max lag values + 2 sorted z-score for evoked)
-    assert _count_combo_items(widget, enabled=False) == 67
+    # (68 plots: spontaneous + evoked + sorted rising edges CCG + 2 CCG z-score plots
+    #  + 2 sorted max lag values + 2 sorted z-score for evoked
+    #  + sorted spike synchrony rising edges)
+    assert _count_combo_items(widget, enabled=False) == 68
 
 
 def test_combo_enabled_with_fov_and_run_id(
